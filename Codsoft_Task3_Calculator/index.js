79677778b5502912c925c5646a48eb59fc29f02b
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-08-23 23:12:47 +0530

    Update index.js

--- Codsoft_Task3_Calculator/index.js
+++ Codsoft_Task3_Calculator/index.js
@@ -21,11 +21,7 @@ function backspc()
     screen.value=screen.value.substr(0,screen.value.length-1);
 }
 
-function mod(){
-    var splitScreen = screen.value.split('%',2);
-    if(splitScreen.length > 1){
-        screen.value=eval(splitScreen[0] + '/100*' + splitScreen[1]);
-    }else{
-        screen.value=eval(screen.value);
-    }
+function percent(){
+    const val=parseFloat(screen.value);
+    screen.value=val/100;
 }
